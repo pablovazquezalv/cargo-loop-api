484a8aa0ffaae98d6086d0b88d20717ec15ae87d
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2025-05-19 22:30:53 -0600

    push

--- resources/views/ResetPasswordMail.blade.php
+++ resources/views/ResetPasswordMail.blade.php
@@ -1,7 +1,7 @@
 <div>
     <h1>Reset Password</h1>
     <p>Click the link below to reset your password:</p>
-    <a >Reset Password</a>
+    <a href="{{ $link }}">Reset Password</a>
     <p>If you did not request a password reset, please ignore this email.</p>
     <p>Thank you!</p>
 </div>
