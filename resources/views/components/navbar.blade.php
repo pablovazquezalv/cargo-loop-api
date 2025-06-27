0263d9b6568cbca225d95a895d35ebab60330f70
Pablo Vazquez Alvarado <96793896+pablovazquezalv@users.noreply.github.com>
2025-06-26 22:12:17 -0600

    cambios a cargo en vez carga

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="flex items-center justify-between px-6 py-4 shadow-sm">
     <div class="text-2xl font-bold">
       <a href="{{ url('/') }}">
-        Carga loop
+        cargo loop
         <img src="/Carga-loop-icon.png" alt="Logo" class="h-8 inline-block" />
       </a>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Carga Loop</title>
+  <title>Cargo Loop</title>
   {{-- ✅ Tailwind CDN --}}
   <script src="https://cdn.tailwindcss.com"></script>
   <style>
